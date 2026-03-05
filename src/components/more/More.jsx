12f1aa679e6c7106cf3b79325ff7ebfb4840ad0f
BahromNigmanbekov <bahromnigmanbekov@gmail.com>
2026-03-05 22:31:24 +0500

    sdafdsf

--- src/components/more/More.jsx
+++ src/components/more/More.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import ProductItem from "../productItem/ProductItem";
+import ProductItem from "./ProductItem";
 import "../products/products.css";
 
 function More() {
